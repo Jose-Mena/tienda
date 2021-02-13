53d56cdb458494f86e4c51d59362b3086b68ac77
jose-mena <>
2021-02-13 11:53:37 -0500

    configurar

--- app/config/configurar.php
+++ app/config/configurar.php
@@ -1,8 +1,8 @@
 <?php
 	define('DB_HOST','localhost');
-	define('DB_USUARIO','postgres');
-	define('DB_PASSWORD','123');
-	define('DB_NOMBRE','votaciones-consejo');
+	define('DB_USUARIO','tienda');
+	define('DB_PASSWORD','');
+	define('DB_NOMBRE','tienda');
 
 	define('RUTA_APP', dirname(dirname(__FILE__)));
 
